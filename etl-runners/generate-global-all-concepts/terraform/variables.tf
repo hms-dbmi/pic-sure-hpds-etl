1df0e643ba0c30dacf498b2aebb726230a37f7db
sharra44 <sharra_neely@hms.harvard.edu>
2026-08-26 19:17:58 -0400

    Revert "Abstracted environment vars"
    This reverts commit 33bc0436c5a170b38d4af823f03e182d47222298.

--- etl-runners/generate-global-all-concepts/terraform/variables.tf
+++ etl-runners/generate-global-all-concepts/terraform/variables.tf
@@ -60,18 +60,6 @@ variable "manage_secret_access" {
   description = "Let this run attach a GetSecretValue policy to the instance role"
 }
 
-variable "rds_host" {
-  type        = string
-  default     = ""
-  description = "RDS endpoint hostname, used when the secret contains only username/password"
-}
-
-variable "rds_dbname" {
-  type        = string
-  default     = ""
-  description = "RDS database name, used when the secret contains only username/password"
-}
-
 variable "tags" {
   type        = map(string)
   default     = {}
